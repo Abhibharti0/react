4461f4a14457066c01095df4dbeca120b4fb37f8
Abhishek Bharti <abhibharti457@gmail.com>
2025-11-04 22:56:19 +0530

    add props

--- components_props_jsx/react_project_two/src/App.jsx
+++ components_props_jsx/react_project_two/src/App.jsx
@@ -1,12 +1,15 @@
-import UserCard from './components/usercard';
+import UserCard from './components/UserCard';
+import img1 from './assets/img1.jpg'
+import img2 from './assets/img2.jpg'
+import img3 from './assets/img3.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className='container'>
-      <UserCard />
-      <UserCard />
-      <UserCard />
+      <UserCard name="Abhishek Bharti"  desc="desc1" image={img1} />
+      <UserCard name="Satyam Dubey"  desc="desc2" image={img2} />
+      <UserCard name="Aditya kumar" desc="desc3"image={img3} />
     </div>
   );
 }
--- components_props_jsx/react_project_two/src/components/usercard.jsx
+++ components_props_jsx/react_project_two/src/components/usercard.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-
+import React from 'react';
 import './usercard.css';
-import img from '../assets/img.png'
+import img from '../assets/img.png';
 
-const usercard = () => {
+const UserCard = (props) => {
   return (
-    <div className='user_container'>
-      <p id='username'>Abhishek Bharti</p>
-      <img id='user_img' src={img} alt="abhi" />
-      <p id='userdesc'>Description of Abhishek Bharti</p>
+    <div className="user_container">
+      <p id="username">{props.name}</p>
+      <img id="user_img" src={props.image} alt={props.name} />
+      <p id="userdesc">{props.desc}</p>
     </div>
-  )
-}
+  );
+};
 
-export default usercard
+export default UserCard;
