a93dfca93b9eca3c076b63df0201fd544321297a
Abhishek Bharti <abhibharti457@gmail.com>
2025-11-05 17:26:34 +0530

    conditional rendering using if&else

---
+++ conditional rendering/crendering/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import Loginbtn from './components/Loginbtn';
+
+import Logoutbtn from './components/Logoutbtn';
+import './App.css';
+
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState(true);
+
+  if (isLoggedIn) {
+    return (<Logoutbtn />)
+  } else {
+    return (<Loginbtn />)
+  }
+}
+
+export default App;
