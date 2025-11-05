64a5d2b14b3518a9b8c654573d452f94df1f30d2
Abhishek Bharti <abhibharti457@gmail.com>
2025-11-05 14:10:56 +0530

    props

---
+++ propsdetail/props/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+ 
+
+  return (
+    <div>
+        <Card name="Abhishek Bharti">
+        <h1>Best Web Dev Course</h1>
+        <p>Trying to be Consistent in this</p>
+        <p>will complete the course sonn</p>
+      </Card>
+    </div>
+  )
+}
+
+export default App
