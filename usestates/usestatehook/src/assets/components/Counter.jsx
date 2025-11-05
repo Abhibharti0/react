268486342df5f0dcba705085a23df8e63174b023
Abhishek Bharti <abhibharti457@gmail.com>
2025-11-05 13:39:19 +0530

    counter using usestate

---
+++ usestates/usestatehook/src/assets/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './Counter.css';
+
+const Counter = () => {
+  const [count, setCount] = useState(13);
+
+  return (
+    <div className="countercontainer">
+      <p id="para">You have clicked {count} times</p>
+      <button id="btn" onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+};
+
+export default Counter;
