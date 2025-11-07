c4e22afbf72b2688522d307b29956895a36d8c98
Abhishek Bharti <abhibharti457@gmail.com>
2025-11-07 21:55:36 +0530

    add validation in code

--- React Hook Form/reacthookform/src/App.jsx
+++ React Hook Form/reacthookform/src/App.jsx
@@ -8,37 +8,84 @@ function App() {
     formState: { errors },
   } = useForm();
 
-  // Form submit hone par ye chalega
+  // Form submit hone par chalega
   const onSubmit = (data) => {
     console.log("Form Data:", data);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      
+      {/* First Name */}
       <div>
         <label>First Name:</label>
         <input
+          className={errors.firstName ? 'input-error' : ''}
           type="text"
-          {...register("firstName", { required: "First Name is required" })}
+          {...register("firstName", {
+            required: "First name is required",
+            minLength: {
+              value: 3,
+              message: "Min length at least 3",
+            },
+            maxLength: {
+              value: 10,
+              message: "Max length at most 10",
+            },
+            pattern: {
+              value: /^[A-Za-z]+$/,
+              message: "Only letters are allowed",
+            },
+          })}
         />
-        {errors.firstName && <p>{errors.firstName.message}</p>}
+        {errors.firstName && (
+          <p className="error-msg">{errors.firstName.message}</p>
+        )}
       </div>
 
+      {/* Middle Name */}
       <div>
         <label>Middle Name:</label>
         <input
+          className={errors.middleName ? 'input-error' : ''}
           type="text"
-          {...register("middleName")}
+          {...register("middleName", {
+            pattern: {
+              value: /^[A-Za-z]+$/,
+              message: "Only letters are allowed",
+            },
+          })}
         />
+        {errors.middleName && (
+          <p className="error-msg">{errors.middleName.message}</p>
+        )}
       </div>
 
+      {/* Last Name */}
       <div>
         <label>Last Name:</label>
         <input
+          className={errors.lastName ? 'input-error' : ''}
           type="text"
-          {...register("lastName", { required: "Last Name is required" })}
+          {...register("lastName", {
+            required: "Last name is required",
+            minLength: {
+              value: 3,
+              message: "Minimum length is 3",
+            },
+            maxLength: {
+              value: 12,
+              message: "Maximum length is 12",
+            },
+            pattern: {
+              value: /^[A-Za-z]+$/,
+              message: "Only letters are allowed",
+            },
+          })}
         />
-        {errors.lastName && <p>{errors.lastName.message}</p>}
+        {errors.lastName && (
+          <p className="error-msg">{errors.lastName.message}</p>
+        )}
       </div>
 
       <button type="submit">Submit</button>
