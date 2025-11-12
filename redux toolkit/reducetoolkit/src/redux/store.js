bdd079cbecb7c68be0764cfff61bdaa2615dc8c0
Abhishek Bharti <abhibharti457@gmail.com>
2025-11-12 15:26:00 +0530

    register reducer in store

--- redux toolkit/reducetoolkit/src/redux/store.js
+++ redux toolkit/reducetoolkit/src/redux/store.js
@@ -1,9 +1,10 @@
 // src/app/store.js
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
 
 
 export const store = configureStore({
   reducer: {
-   
+    counter: counterReducer,
   },
 })
